ddd4d01a0149a8bf01183bffe7d12a0512139d7a
peteraleksa <peter.aleksa@gmail.com>
2014-07-25 15:40:44 -0400

    created results template and view

--- joblistings/search/views.py
+++ joblistings/search/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
 	return render(request, 'home.html')
+
+def results_page(request):
+	return render(request, 'results.html', {
+		'skillset': request.POST.get('skillset', ''),
+	})
