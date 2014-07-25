d0ad9a6068a482c46eb112e89f6d1466ce95ff1b
peteraleksa <peter.aleksa@gmail.com>
2014-07-25 15:42:17 -0400

    created initial models

--- joblistings/search/models.py
+++ joblistings/search/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class JobLink(models.Model):
+	text = models.TextField(default='')
+	post_id = models.AutoField(primary_key=True)
+	summary = models.TextField(default='')
