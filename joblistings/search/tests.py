efe798f2993b864bf41fc49daef9def4c505870e
peteraleksa <peter.aleksa@gmail.com>
2014-07-25 12:18:55 -0400

    Started unit tests and url mapping, dummy view

--- joblistings/search/tests.py
+++ joblistings/search/tests.py
@@ -1,3 +1,11 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from search.views import home_page
 
 # Create your tests here.
+
+class HomePageTest(TestCase):
+
+	def test_root_url_resolves_to_home_page_view(self):
+		round = resolve('/')
+		self.assertEqual(found.func, home_page)
