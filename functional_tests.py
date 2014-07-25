a673ee291a86cd4a6fe2b9d336f3493eee289eca
peteraleksa <peter.aleksa@gmail.com>
2014-07-25 13:22:09 -0400

    expanded functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -15,8 +15,29 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 		self.assertIn('Job Hunt', self.browser.title)
 
-		# open up the settings page
-		# self.assertIn('Settings', self.browser.title)
+		# all skills are selected by default
+		skillset = self.browser.find_element_by_id('skillset')
+		skills = self.browser.find_elements_by_class('skill')
+		for skill in skills:
+			self.assertTrue(selenium.IsChecked(skill))
+
+		# click search button
+		search_button = self.browser.find_element_by_id('search_btn')
+		search_button.click
+
+		# see results with summarys
+		self.browser.implicitly_wait(3)
+		self.assertIn('Results', self.browser.title)
+
+		# change sort by to date posted
+
+		# filter by post date
+
+		# click on result link to see posts
+		result_link = self.browser.find_element_by_class('result_link')
+		result_link.click
+
+		# see a post
 
 		self.fail('Finish the test!')
 
