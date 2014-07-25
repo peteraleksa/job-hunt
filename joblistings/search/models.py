e477b28118f122f7cc126178804e42dc4cd8bd66
peteraleksa <peter.aleksa@gmail.com>
2014-07-25 19:51:49 -0400

    rewrote models for retrieving data from mongo database populated by the spiders

--- joblistings/search/models.py
+++ joblistings/search/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class JobLink(models.Model):
-	text = models.TextField(default='')
-	post_id = models.AutoField(primary_key=True)
-	summary = models.TextField(default='')
+# class JobLink(models.Model):
+# 	text = models.CharField(max_length=50)
+# 	post_id = models.AutoField(primary_key=True)
+# 	summary = models.CharField(max_length=150)
+
+# class JobPost(models.Model):
+# 	title = models.CharField(max_length=50)
+# 	posted = models.CharField(max_length=15) # Date?
+# 	# keywords = ListField() # ???
+# 	original_post_link = models.CharField(max_length=50)
+# 	location = models.CharField(max_length=50)
+# 	job_title = models.CharField(max_length=50)
+# 	address = models.CharField(max_length=50)
+# 	map_link = models.CharField(max_length=50)
+# 	compensation = models.CharField(max_length=50)
+# 	# make relationship
+# 	# skills = ListField()
+# 	# experience = ListField()
+# 	# tokenized_text = ListField() # don't store tokenized text
+# 	text = models.CharField(max_length=500)
+
--- joblistings/search/tests.py
+++ joblistings/search/tests.py
@@ -2,9 +2,14 @@ from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
+
+from pymongo import Connection
+
 from search.views import home_page
 from search.views import results_page
-from search.models import JobLink
+
+#from search.models import JobLink
+#from search.models import JobPost
 
 # Create your tests here.
 
@@ -40,29 +45,93 @@ class HomePageTest(TestCase):
 		)
 		self.assertEqual(response.content.decode(), expected_html)
 
-class JobLinkModelTest(TestCase):
+class DbConnectTest(TestCase):
 
-	def test_saving_and_retrieving_items(self):
-		first_job_link = JobLink()
-		first_job_link.text = "A cool job"
-		#first_job_link.post_id = "1"
-		first_job_link.summary = "This is a job that you totally want to have"
-		first_job_link.save()
+	def test_database_connection(self):
+		databaseName = 'jobHunt-dev'
+		connection = Connection()
 
-		second_job_link = JobLink()
-		second_job_link.text = "A not cool job"
-		#second_job_link.post_id = "2"
-		second_job_link.summary = "You really don't want this job"
-		second_job_link.save()
+		db = connection[databaseName]
+		job_posts = db['jobPosts']
 
-		saved_job_links = JobLink.objects.all()
-		self.assertEqual(saved_job_links.count(), 2)
+		count = job_posts.count()
 
-		first_saved_job_link = saved_job_links[0]
-		second_saved_job_link = saved_job_links[1]
-		self.assertEqual(first_saved_job_link.text, first_job_link.text)
-		self.assertEqual(first_saved_job_link.post_id, 1)
-		self.assertEqual(first_saved_job_link.summary, first_job_link.summary)
-		self.assertEqual(second_saved_job_link.text, second_job_link.text)
-		self.assertEqual(second_saved_job_link.post_id, 2)
-		self.assertEqual(second_saved_job_link.summary, second_job_link.summary)
+		self.assertIsNotNone(count)
+
+	#def test_database_lookup(self):
+
+#class JobLinkModelTest(TestCase):
+
+	# def test_saving_and_retrieving_job_links(self):
+		# first_job_link = JobLink()
+		# first_job_link.text = 'A cool job'
+		# #first_job_link.post_id = "1"
+		# first_job_link.summary = 'This is a job that you totally want to have'
+		# first_job_link.save()
+
+		# second_job_link = JobLink()
+		# second_job_link.text = 'A not cool job'
+		# #second_job_link.post_id = "2"
+		# second_job_link.summary = 'You really don\'t want this job'
+		# second_job_link.save()
+
+		# saved_job_links = JobLink.objects.all()
+		# self.assertEqual(saved_job_links.count(), 2)
+
+		# first_saved_job_link = saved_job_links[0]
+		# second_saved_job_link = saved_job_links[1]
+		# self.assertEqual(first_saved_job_link.text, first_job_link.text)
+		# self.assertEqual(first_saved_job_link.post_id, 1)
+		# self.assertEqual(first_saved_job_link.summary, first_job_link.summary)
+		# self.assertEqual(second_saved_job_link.text, second_job_link.text)
+		# self.assertEqual(second_saved_job_link.post_id, 2)
+		# self.assertEqual(second_saved_job_link.summary, second_job_link.summary)
+
+	# def test_saving_and_retrieving_job_posts(self):
+		# title = 'First Job'
+		# posted = '2014-07-22  5:21pm'
+		# keywords = ['c#', 'java', 'python']
+		# original_post_link = 'http://localhost:8000'
+		# location = ''
+		# job_title = ''
+		# address = ''
+		# map_link = ''
+		# compensation = ''
+		# skills = []
+		# experience = []
+		# tokenized_text = ['Some', 'text', 'about', 'the', 'job']
+		# text = 'Some text about the job'
+
+		# first_job_post = JobPost()
+		# first_job_post.title = title
+		# first_job_post.keywords = keywords
+		# first_job_post.original_post_link = original_post_link
+		# first_job_post.location = location
+		# first_job_post.job_title = job_title
+		# first_job_post.address = address
+		# first_job_post.map_link = map_link
+		# first_job_post.compensation = compensation
+		# first_job_post.skills = skills
+		# first_job_post.experience = experience
+		# first_job_post.tokenized_text = tokenized_text
+		# first_job_post.text = text
+
+		# first_job_id = first_job_post.save()
+
+		# saved_job_posts = JobPosts.objects.findOne()
+		# self.assertEqual(saved_job_posts.count(), 1)
+
+		# first_saved_job_post = saved_job_posts[0]
+
+		# self.assertEqual(first_saved_job_post.title, title)
+		# self.assertEqual(first_saved_job_post.keywords, keywords)
+		# self.assertEqual(first_saved_job_post.original_post_link, original_post_link)
+		# self.assertEqual(first_saved_job_post.location, location)
+		# self.assertEqual(first_saved_job_post.job_title, job_title)
+		# self.assertEqual(first_saved_job_post.address, address)
+		# self.assertEqual(first_saved_job_post.map_link, map_link)
+		# self.assertEqual(first_saved_job_post.compensation, compensation)
+		# self.assertEqual(first_saved_job_post.skills, skills)
+		# self.assertEqual(first_saved_job_post.experience, experience)
+		# self.assertEqual(first_saved_job_post.tokenized_text, tokenized_text)
+		# self.assertEqual(first_saved_job_post.text, text)
