18c159d28cca03811e3cf0bc8de1be8b2c66a241
peteraleksa <peter.aleksa@gmail.com>
2014-07-26 18:24:50 -0400

    added results lookup

--- joblistings/search/views.py
+++ joblistings/search/views.py
@@ -8,10 +8,30 @@ def home_page(request):
 
 		skillset = request.GET.getlist('skillset')
 
-		#jobs = JobPost.objects(skills__in=skillset)[0:15]
+		jobs = []
+		job_ids = []
+		db_skills = []
+
+		for skill in skillset:
+			db_skills.extend(Skill.objects.filter(skill__exact=skill.lower()))
+
+		for dbs in db_skills:
+			ids = JobSkills.objects.filter(skill_id__exact=dbs._id)
+			for i in ids:
+				if i.post_id not in job_ids:
+					job_ids.extend(i)
+
+		count = 0
+		for jid in job_ids:
+			if count < 10:
+				jobs.extend(JobPosts.objects.filter(_id__exact=jid))
+				count+=1
+			else:
+				break
 
 		return render(request, 'home.html', {
 			'skillset': skillset,
+			'jobs': jobs,
 		})
 
 	return render(request, 'home.html')
