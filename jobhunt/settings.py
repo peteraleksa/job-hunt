665d963725fca1ac38dfe404ea27d5fc64d764a3
peteraleksa <peter.aleksa@gmail.com>
2014-07-27 11:09:30 -0400

    changed to reflect mysql db, pipeline

--- jobhunt/settings.py
+++ jobhunt/settings.py
@@ -6,19 +6,31 @@
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
 
+BOT_NAME = 'craigslist'
+
 SPIDER_MODULES = ['jobhunt.spiders']
 NEWSPIDER_MODULE = 'jobhunt.spiders'
 
 DOWNLOAD_DELAY = 2
 CONCURRENT_REQUESTS_PER_DOMAIN = 4
 
-MONGODB_URI = 'mongodb://localhost:27017'
-MONGODB_DATABASE = 'jobHunt-dev'
-MONGODB_COLLECTION = 'jobPosts'
+DATABASE = {
+	'drivername': 'mysql',
+	'host': 'localhost',
+	'port': '5432',
+	'username': 'jobhunt',
+	'password': 'jobhunt',
+	'database': 'jobHunt'
+}
+
+#MONGODB_URI = 'mongodb://localhost:27017'
+#MONGODB_DATABASE = 'jobHunt-dev'
+#MONGODB_COLLECTION = 'jobPosts'
 #MONGODB_UNIQUE_KEY = 'postId'
 
 ITEM_PIPELINES = [
-	'scrapy_mongodb.MongoDBPipeline'
+	'jobhunt.pipelines.JobhuntPipeline'
+#	'scrapy_mongodb.MongoDBPipeline'
 ]
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
